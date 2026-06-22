5962131a407bc206690ea825073a2909f208c315
Othavio Nogueira <othavionogueira2003@gmail.com>
2026-06-22 20:23:50 -0300

    feat: create enums for account

---
+++ src/main/java/com/kage/account/domain/enums/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.kage.account.domain.enums;
+
+public enum AccountStatus {
+    ACTIVE, BLOCKED, CLOSED
+}
---
+++ src/main/java/com/kage/account/domain/enums/AccountType.java
@@ -0,0 +1,5 @@
+package com.kage.account.domain.enums;
+
+public enum AccountType {
+    CHECKING, SAVINGS
+}
